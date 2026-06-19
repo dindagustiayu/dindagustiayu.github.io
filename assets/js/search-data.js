41e1511a42d702b68f076fccb73783500c31adb7
dindagustiayu <dindagustiayuedu@gmail.com>
2026-06-19 06:09:48 +0000

    Deploying to gh-pages from @ dindagustiayu/dindagustiayu.github.io@a9a3ca687981669cb65ab01bed284f49e4419d5d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -35,7 +35,7 @@ ninja.data = [{
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/_pages/";
+            window.location.href = "/hobbies/";
           },
         },{id: "post-python-for-scientific-computing-and-data-visualizations-eigen-spaces",
         
